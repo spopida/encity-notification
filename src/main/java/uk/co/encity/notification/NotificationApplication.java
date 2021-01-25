20e86e2ebcbdad937073c02d604fa33ac4f6bc66
Adrian Hall <ade@adrian-hall.name>
2021-01-25 11:16:04 +0000

    Initial project setup

---
+++ src/main/java/uk/co/encity/notification/NotificationApplication.java
@@ -0,0 +1,13 @@
+package uk.co.encity.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationApplication.class, args);
+	}
+
+}
